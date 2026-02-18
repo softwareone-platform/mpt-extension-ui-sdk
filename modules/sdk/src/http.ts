00e2890a5edb577fd195b55943a1336478b19112
Anton Hinz <anton.hinz@softwareone.com>
2026-02-18 09:46:42 +0100

    Add http client

---
+++ modules/sdk/src/http.ts
@@ -0,0 +1,56 @@
+import axios from 'axios';
+
+export const REQUEST_TOKEN_EVENT = '$requesttoken' as const;
+export const REPORT_TOKEN_EVENT = '$reporttoken' as const;
+
+function base64Decode(str: string) {
+    str = str.replace(/-/g, "+").replace(/_/g, "/");
+    const pad = str.length % 4;
+    if (pad) str += "=".repeat(4 - pad);
+
+    // bytes -> utf-8 string
+    const binary = atob(str);
+    const bytes = Uint8Array.from(binary, c => c.charCodeAt(0));
+    return new TextDecoder().decode(bytes);
+}
+
+export function parseJWT(token: string) {
+    const [,p] = token.split(".");
+    return JSON.parse(base64Decode(p));
+}
+
+const isExpired = (jwt: string) => {
+    if (!jwt) return true;
+    const { exp } = parseJWT(jwt);
+    return exp - Date.now() > 60 * 1000;
+}
+
+const http = (() => {
+    const emit = (window as any).__MPT__?.emit;
+    const listen = (window as any).__MPT__?.listen;
+
+    let $token = null;
+    const client = axios.create();
+
+    client.interceptors.request.use(async (config) => {
+        if (isExpired($token)) {
+            $token = await new Promise<string>((resolve) => {
+                const stop = listen(REPORT_TOKEN_EVENT, ({ token }) => {
+                    stop();
+                    resolve(token);
+                });
+
+                emit(REQUEST_TOKEN_EVENT);
+            });
+        }
+
+        (config.headers as any).Authorization = `Bearer ${$token}`;
+
+        return config;
+    })
+
+    return client;
+})();
+
+export default http;
+
